4649b4061f1614b2d5db5734e7bb20f819d14241
Dikshat <dikshatsingh64@gmail.com>
2021-08-31 09:47:39 +0530

    changed the JS animation. Now whenever each skill-bar becomes visible on the screen, it will get filled automatically independent of other skill-bars.

--- script.js
+++ script.js
@@ -1,32 +1,40 @@
 "use strict";
-var skillsDiv = document.querySelector("#skills");
-var skillBars = document.querySelectorAll(".skill-bar")
 
-function skillAnimation() {
-    for (let i = 0; i < skillBars.length; i++) {
-        let skillLevel = skillBars[i].getAttribute("data-skill-level");
-        let currWidth = 0;
-        let skillInterval = setInterval(function () {
-            if (currWidth >= skillLevel) {
-                clearInterval(skillInterval);
-                return;
-            } else {
-                currWidth += 1;
-                skillBars[i].style.width = currWidth + "%";
-            }
-        }, 10);
-    }
+function fillBar(skillBar) {
+    let skillLevel = skillBar.getAttribute("data-skill-level");
+    let currWidth = 0;
+    let skillInterval = setInterval(function () {
+        if (currWidth >= skillLevel) {
+            clearInterval(skillInterval);
+            return;
+        } else {
+            currWidth += 1;
+            skillBar.style.width = currWidth + "%";
+        }
+    }, 12); //Here 12ms is the time taken to fill 1% width of the skill-bar.
 }
 
-function sectionVisible() {
+var skillContainers = document.getElementsByClassName('skill-container');
+var len = skillContainers.length;
+var isFilled = new Array(len);  /*For keeping record whether skill bar has been filled or not*/
+
+for(let i = 0;i<skillContainers.length;i++){
+    isFilled[i] = false;
+    window.addEventListener('scroll', function(){
+        skillVisible(skillContainers[i], i); 
+    });
+}
+
+/*Below function will trigger the animation when any skill-container becomes visible*/
+function skillVisible(skillsDiv, idx) {
     var coordinates = skillsDiv.getBoundingClientRect();
     var verticalDist = coordinates.top;
-    // console.log("verticalDist =", verticalDist);
-    if (verticalDist <= 300) {
-        skillAnimation();
-        clearInterval(id);
+
+    if (isFilled[idx] == false && verticalDist <= window.innerHeight) {
+        console.log(skillBar);
+        var skillBar = skillsDiv.querySelector(".skill-bar");
+        fillBar(skillBar);
+        isFilled[idx] = true;   /*Marking the skill bar as filled*/
         return;
     }
 }
-
-var id = setInterval(sectionVisible, 50);
